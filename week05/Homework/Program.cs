f5b14e49425e77c02ef5055f770682abe0e0a024
em-larson <elarson1@byupathway.edu>
2025-06-05 19:02:36 -0600

    finished weekly practice assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Harry Potter", "Divination: tea Leaves");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Hermione Granger", "Arithmancy", "Section 7", "12-25 odd");
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine(assignment2.GetSummary());
+
+        WritingAssignment assignment3 = new WritingAssignment("Ron Weasly", "Quidditch", "Why I Should be a Keeper");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
