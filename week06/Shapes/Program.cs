90e7eadaed27c2695b16dd927310044d64058c66
em-larson <elarson1@byupathway.edu>
2025-06-12 12:55:28 -0600

    Finished week 6 Homework

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("blue", 5);
+
+        Rectangle rectangle = new Rectangle("green", 2.5, 3);
+
+        Circle circle = new Circle("orange", 6.324);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
